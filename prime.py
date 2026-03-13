e7677598f1f67cd0eb740a4fd7df5fbed840be9d
sanikannaik <sanikannaiksanikanaik@gmail.com>
2026-03-13 19:35:46 +0530

    Delete prime.py

--- prime.py
+++
@@ -1,9 +0,0 @@
-num=int(input("Enter the number: "))
-if num>1:
-  for i in range(2,num):
-    if num%i==0:
-      print("Not a prime number")
-    else:
-      print("Prime number")
-else:
-  print("Not a prime number")
